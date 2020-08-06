7445ced95446f73ad40a4baa146fce61ed988468
SimoneCagol <57213014+SimoneCagol@users.noreply.github.com>
2020-08-06 11:38:03 +0200

    fix index

--- PGAPI/index.php
+++ PGAPI/index.php
@@ -7,25 +7,10 @@ require 'vendor/autoload.php';
 error_reporting(E_ERROR);
 require 'class.php';
 
-Flight::route('/number/@number(/page/@page)', function($number, $page = false){
-    $p = new PGAPI("telefono-".$number,$page);
-});
 
-Flight::route('/company/@name(/place/@place)(/page/@page)', function($name,$place = false, $page = false){
-    $p = new PGAPI($name.(($place) ? "/".$place : ""), $page);
-});
 
-Flight::route('/place/@place(/page/@page)', function($place, $page = false){
-    $p = new PGAPI("indirizzo-".$place, $page);
-});
+$p = new PGAPI("telefono-0461913853",$page);
 
-Flight::route('/category/@category(/page/@page)', function($category, $page = false){
-    $p = new PGAPI("cat-".$category, $page);
-});
-
-Flight::route('/search/@term(/page/@page)', function($term, $page = false){
-    $p = new PGAPI($term, $page);
-});
 
 Flight::route('/*',function(){
     echo json_encode(array("status" => "ERROR", "errorDescription" => "Invalid Path"),JSON_PRETTY_PRINT);
